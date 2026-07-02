da152eae810672c97d23be02d89e2e0dc933fd8f
Vladimir Ivakhnenko <c-basso@ya.ru>
2026-07-02 11:48:31 +0300

    wip

--- build/constants.js
+++ build/constants.js
@@ -108,14 +108,30 @@ const YANDEX_METRIKA_COUNTER_ID = 102522439;
 
 const YANDEX_METRIKA_SNIPPET = `  <!-- Yandex.Metrika counter -->
   <script type="text/javascript">
-    (function(m,e,t,r,i,k,a){
-        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-        m[i].l=1*new Date();
-        for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
+    (function() {
+      function initMetrika() {
+        (function(m,e,t,r,i,k,a){
+          m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+        })(window, document,'script','https://mc.yandex.ru/metrika/tag.js', 'ym');
 
-    ym(${YANDEX_METRIKA_COUNTER_ID}, 'init', {clickmap:true, referrer: document.referrer, url: location.href, accurateTrackBounce:true, trackLinks:true});
+        ym(${YANDEX_METRIKA_COUNTER_ID}, 'init', {
+          clickmap: true,
+          trackLinks: true,
+          accurateTrackBounce: true,
+          defer: true,
+          referrer: document.referrer,
+          url: location.href
+        });
+      }
+      if ('requestIdleCallback' in window) {
+        requestIdleCallback(initMetrika, { timeout: 2000 });
+      } else {
+        window.addEventListener('load', initMetrika, { once: true });
+      }
+    })();
   </script>
   <noscript><div><img src="https://mc.yandex.ru/watch/${YANDEX_METRIKA_COUNTER_ID}" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
   <!-- /Yandex.Metrika counter -->`;
